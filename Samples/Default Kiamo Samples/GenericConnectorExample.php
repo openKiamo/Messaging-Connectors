952eda0d45523693d3a8357d123a2ea2248b5668
Sebastien Iniesta <sebastien.iniesta@gmail.com>
2019-10-22 14:51:37 +0200

    20191022 : Default Kiamo Samples

---
+++ Samples/Default Kiamo Samples/GenericConnectorExample.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace UserFiles\Messaging\Connector;
+
+use Kiamo\Bundle\AdminBundle\Utility\Messaging\ConnectorConfiguration;
+use Kiamo\Bundle\AdminBundle\Utility\Messaging\GenericConnectorInterface;
+
+/**
+ * Class TestGenericConnector
+ * @package Kiamo\Bundle\AdminBundle\Utility\Messaging
+ */
+class GenericConnectorExample implements GenericConnectorInterface
+{
+    /**
+     * @var ConnectorConfiguration
+     */
+    private $_parameters;
+
+    /**
+     * @param ConnectorConfiguration $configuration
+     */
+    public function __construct(ConnectorConfiguration $configuration) {
+        $this->_parameters = $configuration;
+    }
+
+    /**
+     * Retourne le nom affiché dans l'interface Kiamo
+     * @return string
+     */
+    public function getName() {
+        return 'Nom affiché dans Kiamo';
+    }
+
+    /**
+     * Récupération des messages
+     * @param \Kiamo\Bundle\AdminBundle\Utility\Messaging\ParameterBag $parameterBag
+     * @return \Kiamo\Bundle\AdminBundle\Utility\Messaging\ParameterBag
+     */
+    public function fetch($parameterBag) {
+        $parameterBag->addMessage([
+            'id' => 'ID du message',
+            'senderId' => 'ID du contact',
+            'senderName' => 'Nom du contact',
+            'content' => 'Contenu du message',
+        ]);
+
+        return $parameterBag;
+    }
+
+    /**
+     * Send a message
+     * @param array $messageTask
+     * @return void
+     */
+    public function send(array $messageTask) {
+        // Logique d'envoi d'un message
+    }
+}
